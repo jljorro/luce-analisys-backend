8aa8aa75ee767791e41aefc62026b6656d62e75e
Jose L. Jorro Aragoneses <jljorro@ucm.es>
2022-05-18 19:38:37 +0200

    Fixed cors call

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors) // Para evitar error CORS
+app.use(cors()) // Para evitar error CORS
 app.use(express.json())
 
 let activites = [
